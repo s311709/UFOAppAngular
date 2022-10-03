6b1d17f698051ba5d591ffe1ca7f9baeed0ae9d4
s311709 <piaerenmia@gmail.com>
2022-10-03 13:36:26 +0200

    registrerte observasjoner works!

--- UFOAppAngular/ClientApp/src/app/registrerte-observasjoner/registrerte-observasjoner.component.ts
+++ UFOAppAngular/ClientApp/src/app/registrerte-observasjoner/registrerte-observasjoner.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-index',
+  selector: 'app-registrerte-observasjoner',
   templateUrl: './registrerte-observasjoner.component.html',
   styleUrls: ['./registrerte-observasjoner.component.css']
 })
