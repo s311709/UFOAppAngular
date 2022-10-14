91f782893f4a3986475b97687f63d70fb42c799b
s311709 <piaerenmia@gmail.com>
2022-10-14 09:54:33 +0200

    pauser med skjult nav-meny

--- UFOAppAngular/ClientApp/src/app/nav-meny/nav-meny.component.ts
+++ UFOAppAngular/ClientApp/src/app/nav-meny/nav-meny.component.ts
@@ -2,28 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-nav-meny',
-  templateUrl: './nav-meny.component.html',
-  styleUrls: ['./nav-meny.component.css']
+    selector: 'app-nav-meny',
+    templateUrl: './nav-meny.component.html',
+    styleUrls: ['./nav-meny.component.css']
 })
 export class NavMenyComponent implements OnInit {
 
-  loggetInn: boolean;
+    loggetInn: boolean;
 
-    ngOnInit(){
+    ngOnInit() {
         this.sjekkLoggetInn();
     }
 
     constructor(private http: HttpClient) { }
 
     sjekkLoggetInn() {
-        
+
         this.http.get<any>("api/UFO/SjekkLoggetInn")
             .subscribe(retur => {
                 if (retur == true) {
-                    document.getElementById("skjultNav").hidden = true;
+                    console.log("logget inn nå");
+                    document.getElementById("#loggetInn").hidden = false;
                 } else if (retur == false) {
-                    document.getElementById("skjultNav").hidden = false;
+                    console.log("ikke logget inn nå");
+                    document.getElementById("#loggetInn").hidden = true;
+
                 }
             },
                 error => console.log(error)
@@ -31,3 +34,16 @@ export class NavMenyComponent implements OnInit {
     }
 
 }
+
+/*
+if (retur == true) {
+    const navElement = document.querySelectorAll<HTMLElement>(".skjultNav")!;
+    navElement.forEach(function (element) {
+        (element as HTMLElement).hidden = true;
+    })
+} else if (retur == false) {
+    const navElement = document.querySelectorAll<HTMLElement>(".skjultNav")!;
+    navElement.forEach(function (element) {
+        (element as HTMLElement).hidden = false;
+    }) 
+    */
